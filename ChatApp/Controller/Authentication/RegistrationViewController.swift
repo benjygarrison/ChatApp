78493662ee7ebed98fe217ac3e030521ccb3716d
benjygarrison <46270616+benjygarrison@users.noreply.github.com>
2022-01-21 00:34:03 -0800

    formatted add photo button

--- ChatApp/Controller/Authentication/RegistrationViewController.swift
+++ ChatApp/Controller/Authentication/RegistrationViewController.swift
@@ -11,11 +11,47 @@ class RegistrationViewController: UIViewController {
     
     //MARK: Properties
     
+    private let addPhotoButton: UIButton = {
+        let addPhotoButton = UIButton(type: .system)
+        //addPhotoButton.setImage(UIImage(systemName: "person.crop.circle"), for: .normal)
+        addPhotoButton.tintColor = .white
+        addPhotoButton.addTarget(self, action: #selector(addNewPhoto), for: .touchUpInside)
+        return addPhotoButton
+    }()
+    
     
     //MARK: ViewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
+        
+        setUpUI()
+        
     }
+    
+    //MARK: Layout
+    
+    private func setUpUI() {
+        setUpGradient()
+        
+        view.addSubview(addPhotoButton)
+        addPhotoButton.centerX(inView: view)
+        addPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        addPhotoButton.backgroundColor = .clear
+        addPhotoButton.layer.borderWidth = 3.0
+        addPhotoButton.layer.borderColor = UIColor.white.cgColor
+        addPhotoButton.layer.cornerRadius = 60
+        addPhotoButton.setTitle("+", for: .normal)
+        addPhotoButton.titleLabel?.font = UIFont.systemFont(ofSize: 75, weight: .regular)
+        addPhotoButton.setDimensions(height: 120, width: 120)
+
+    }
+    
+    
+    //MARK: Selectors
+    
+    @objc private func addNewPhoto() {
+        print("photo added")
+    }
+    
 }
